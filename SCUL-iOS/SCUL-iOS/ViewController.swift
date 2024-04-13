ea5ea507e95a3a4170229ce5c7173d7dac588907
ray3238 <jjmh209@gmail.com>
2024-04-14 00:40:34 +0900

    🌟 :: 프로젝트 세팅

---
+++ SCUL-iOS/SCUL-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCUL-iOS
+//
+//  Created by 이태규 on 4/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
