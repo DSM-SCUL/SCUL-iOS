32d1439c3cdb5f00a0185eb35b7a69d46a0621aa
ray3238 <jjmh209@gmail.com>
2024-04-21 04:32:11 +0900

    🌟 :: Font 코드 추가

---
+++ SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Font/UIFont+Heading.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+public extension UIFont {
+    //MARK: Heading
+    static let heading1 = UIFont(name: "WantedSans-SemiBold", size: 42) ?? UIFont.systemFont(ofSize: 42)
+    static let heading2 = UIFont(name: "WantedSans-SemiBold", size: 32) ?? UIFont.systemFont(ofSize: 32)
+    static let heading3 = UIFont(name: "WantedSans-SemiBold", size: 24) ?? UIFont.systemFont(ofSize: 24)
+    static let heading4 = UIFont(name: "WantedSans-SemiBold", size: 16) ?? UIFont.systemFont(ofSize: 16)
+}
