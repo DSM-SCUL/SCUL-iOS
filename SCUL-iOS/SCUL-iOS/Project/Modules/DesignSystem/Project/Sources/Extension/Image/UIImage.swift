b1348395fe663f426e0ae7adf46546ea494ee4c0
ray3238 <jjmh209@gmail.com>
2024-04-24 09:41:19 +0900

    Revert "🌟 :: Color 추가"
    This reverts commit ca9d009edc816994b0da32462e2dd572eb9fbc64.

--- SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Image/UIImage.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-public extension UIImage {
-    //MARK: Logo
-    static let SculLogo = UIImage(named: "SculLogo")
-
-    //MARK: Banner
-    static let SculBanner = UIImage(named: "BannerImage")
-}
