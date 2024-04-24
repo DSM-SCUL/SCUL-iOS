ca9d009edc816994b0da32462e2dd572eb9fbc64
ray3238 <jjmh209@gmail.com>
2024-04-24 09:06:54 +0900

    🌟 :: Color 추가

---
+++ SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Color/UIColor + GrayScale.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+public extension UIColor {
+    //MARK: GrayScale
+    static let Gray50: UIColor = UIColor(named: "Gray50")!
+    static let Gray100: UIColor = UIColor(named: "Gray100")!
+    static let Gray200: UIColor = UIColor(named: "Gray200")!
+    static let Gray300: UIColor = UIColor(named: "Gray300")!
+    static let Gray400: UIColor = UIColor(named: "Gray400")!
+    static let Gray500: UIColor = UIColor(named: "Gray500")!
+    static let Gray600: UIColor = UIColor(named: "Gray600")!
+    static let Gray700: UIColor = UIColor(named: "Gray700")!
+    static let Gray800: UIColor = UIColor(named: "Gray800")!
+    static let Gray900: UIColor = UIColor(named: "Gray900")!
+}
---
+++ SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Color/UIColor + Main.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+public extension UIColor {
+    //MARK: Main
+    static let Main100: UIColor = UIColor(named: "Main100")!
+    static let Main200: UIColor = UIColor(named: "Main200")!
+    static let Main300: UIColor = UIColor(named: "Main300")!
+    static let Main400: UIColor = UIColor(named: "Main400")!
+    static let Main500: UIColor = UIColor(named: "Main500")!
+    static let Main600: UIColor = UIColor(named: "Main600")!
+    static let Main700: UIColor = UIColor(named: "Main700")!
+    static let Main800: UIColor = UIColor(named: "Main800")!
+}
