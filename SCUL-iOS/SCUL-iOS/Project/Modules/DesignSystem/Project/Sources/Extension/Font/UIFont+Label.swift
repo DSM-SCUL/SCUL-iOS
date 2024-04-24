a258b8800f8f35f7595897ebc1eecce962cfba7e
ray3238 <jjmh209@gmail.com>
2024-04-24 09:49:31 +0900

    🌟 :: 더미커밋

---
+++ SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Font/UIFont+Label.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public extension UIFont {
+    //MARK: Label
+    static let label1 = UIFont(name: "WantedSans-Medium", size: 16) ?? UIFont.systemFont(ofSize: 16)
+    static let label2 = UIFont(name: "WantedSans-Medium", size: 12) ?? UIFont.systemFont(ofSize: 12)
+}
---
+++ SCUL-iOS/SCUL-iOS/Project/Presentation/MyPage/MyPageViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SnapKit
+import Then
+
+class MyPageViewController: BaseViewController<MyPageViewModel> {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addView()
+        setLayout()
+    }
+
+    public override func addView() {}
+
+    public override func setLayout() {}
+
+    public override func bind() {}
+
+    public override func configureViewController() {}
+
+    public override func configureNavigation() {}
+}
