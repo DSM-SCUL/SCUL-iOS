5d3b3aa14ada5754de3eb1bd63f86c7e75848dd2
ray3238 <jjmh209@gmail.com>
2024-04-24 10:01:22 +0900

    커밋

--- SCUL-iOS/SCUL-iOS/Project/App/Sources/SceneDelegate.swift
+++ SCUL-iOS/SCUL-iOS/Project/App/Sources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let viewController = ViewController(ViewModel())
+        let viewController = SplashViewController(SplashViewModel())
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
