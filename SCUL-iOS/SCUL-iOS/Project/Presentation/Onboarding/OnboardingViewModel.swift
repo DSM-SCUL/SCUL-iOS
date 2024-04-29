e3e751cd6080806a4783184970efa8cfa0e5348a
ray3238 <jjmh209@gmail.com>
2024-04-29 16:14:03 +0900

    🌱 :: Onboarding 퍼블리싱

--- SCUL-iOS/SCUL-iOS/Project/Presentation/Onboarding/OnboardingViewModel.swift
+++ SCUL-iOS/SCUL-iOS/Project/Presentation/Onboarding/OnboardingViewModel.swift
@@ -7,13 +7,22 @@ public final class OnboardingViewModel: BaseViewModel, Stepper {
     public let steps = PublishRelay<Step>()
     private let disposeBag = DisposeBag()
 
-//    init( ) { }
+    init( ) { }
 
-    public struct Input { }
+    public struct Input {
+        let navigateToLoginButtonDidTap: PublishRelay<Void>
+    }
 
     public struct Output { }
 
     public func transform(_ input: Input) -> Output {
+        input.navigateToLoginButtonDidTap.asObservable()
+            .map {
+                OnboardingStep.loginIsRequired
+            }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+
         return Output( )
     }
 }
