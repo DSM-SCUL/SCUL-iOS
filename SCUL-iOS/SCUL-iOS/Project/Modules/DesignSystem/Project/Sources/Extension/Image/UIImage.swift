ee0535a49acc9c665695b801415ca41d1b031673
ray3238 <jjmh209@gmail.com>
2024-04-29 16:12:29 +0900

    🌱 :: bookmarkImage 추가

--- SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Image/UIImage.swift
+++ SCUL-iOS/SCUL-iOS/Project/Modules/DesignSystem/Project/Sources/Extension/Image/UIImage.swift
@@ -3,7 +3,12 @@ import UIKit
 public extension UIImage {
     //MARK: Logo
     static let SculLogo = UIImage(named: "SculLogo")
+    static let SmallSculLogo = UIImage(named: "SmallSculLogo")
 
     //MARK: Banner
     static let SculBanner = UIImage(named: "BannerImage")
+
+    //MARK: Bookmark
+    static let BookmarkOff = UIImage(named: "BookmarkOff")
+    static let BookmarkOn = UIImage(named: "BookmarkOn")
 }
