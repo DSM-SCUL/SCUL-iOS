5eed1445c54cb4f502340cee37ecdb6f64331365
ray3238 <jjmh209@gmail.com>
2024-05-05 16:36:03 +0900

    🌱 :: 나의 리뷰 퍼블리싱

---
+++ SCUL-iOS/SCUL-iOS/Project/Presentation/MyReview/Component/MyReviewTableViewCell.swift
@@ -0,0 +1,60 @@
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+
+class MyReviewTableViewCell: BaseTableViewCell<MyReviewViewModel> {
+    static let identifier = "MyReviewTableViewCell"
+    private var disposeBag = DisposeBag()
+
+    private let placeLabel = UILabel().then {
+        $0.labelSetting(text: "서울 시립 미술관에서", font: .caption1)
+        $0.textColor = .black
+    }
+    private let nameLabel = UILabel().then {
+        $0.labelSetting(text: "강*민", font: .body1)
+        $0.textColor = .Gray700
+    }
+    private let dateLabel = UILabel().then {
+        $0.labelSetting(text: "2024.04.15", font: .body1)
+        $0.textColor = .Gray700
+    }
+    private let contentLabel = UILabel().then {
+        $0.labelSetting(text: "sdksmakdfnklsanfklsakldsadmfkldsanjkgnadkfgnlkdasjngbljdsajgkndaskjgnadsjklgnjkadsngkjldsngljkadsnljkfmdsklafmk", font: .body2)
+        $0.textColor = .black
+    }
+
+    override func addView() {
+        [
+            placeLabel,
+            nameLabel,
+            dateLabel,
+            contentLabel
+        ].forEach { contentView.addSubview($0) }
+    }
+
+    override func setLayout() {
+        placeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(28)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(placeLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(placeLabel.snp.bottom).offset(20)
+            $0.leading.equalTo(nameLabel.snp.trailing).offset(20)
+        }
+
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(16)
+            $0.leading.trailing.bottom.equalToSuperview().inset(20)
+        }
+    }
+
+    override func configureView() {}
+}
--- SCUL-iOS/SCUL-iOS/Project/Presentation/MyReview/MyReviewViewController.swift
+++ SCUL-iOS/SCUL-iOS/Project/Presentation/MyReview/MyReviewViewController.swift
@@ -3,19 +3,68 @@ import SnapKit
 import Then
 
 class MyReviewViewController: BaseViewController<MyReviewViewModel> {
+    private let myReviewLabel = UILabel().then {
+        $0.labelSetting(text: "내가 작성한 리뷰", font: .sb1)
+    }
+    private let myReviewTableView = UITableView().then {
+        $0.register(
+            MyReviewTableViewCell.self,
+            forCellReuseIdentifier: MyReviewTableViewCell.identifier
+        )
+        $0.estimatedRowHeight = 110
+        $0.rowHeight = UITableView.automaticDimension
+        $0.showsVerticalScrollIndicator = false
+        $0.layer.borderWidth = 0.5
+        $0.layer.borderColor = UIColor.Gray100.cgColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         addView()
         setLayout()
     }
 
-    public override func addView() {}
+    public override func addView() {
+        [
+            myReviewLabel,
+            myReviewTableView
+        ].forEach { self.view.addSubview($0) }
+    }
 
-    public override func setLayout() {}
+    public override func setLayout() {
+        myReviewLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(120)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        myReviewTableView.snp.makeConstraints {
+            $0.top.equalTo(myReviewLabel.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 
     public override func bind() {}
 
-    public override func configureViewController() {}
+    public override func configureViewController() {
+        myReviewTableView.dataSource = self
+        myReviewTableView.delegate = self
+        myReviewTableView.estimatedSectionHeaderHeight = 0
+    }
 
     public override func configureNavigation() {}
+
+    override func viewDidAppear(_ animated: Bool) {
+        myReviewTableView.reloadData()
+    }
+}
+
+extension MyReviewViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MyReviewTableViewCell.identifier, for: indexPath) as! MyReviewTableViewCell
+
+        return cell
+    }
 }
